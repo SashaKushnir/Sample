60360c576e66754fb77f366f39a385d9dceccb94
Sasha <oleksandr.kushnir.ki.2018@lpnu.ua>
2021-03-14 17:04:09 +0200

    last changes

--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -3,21 +3,23 @@ import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import s from './Content.module.css'
 import { CreateNew } from "../CreateNew/CreateNew";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 
 export const MyPage = () => {
+    let {url} = useRouteMatch()
     return <div className={s.wrap}>
         <div className={s.content}>
             <Header/>
             <Switch>
-                <Route path='/content/new' render={() => <CreateNew/>}/>
-                <Route path='/content/history' render={() => <div>history</div>}/>
-                <Route path='/content/editors' render={() => <div>editors</div>}/>
-                <Route path='/content/reports' render={() => <div>reports</div>}/>
-                <Route path='/content/support' render={() => <div>support</div>}/>
-                <Route path='/content/new' render={() => <div>block</div>}/>
-                <Route path='/content/block' render={() => <div>block</div>}/>
-                <Route path='/content/authors' render={() => <div>authors</div>}/>
+                <Redirect exact from={`${url}`} to={`${url}/history`} />
+                <Route path={`${url}/new`} render={() => <CreateNew/>}/>
+                <Route path={`${url}/history`} render={() => <div>history</div>}/>
+                <Route path={`${url}/editors`} render={() => <div>editors</div>}/>
+                <Route path={`${url}/reports`} render={() => <div>reports</div>}/>
+                <Route path={`${url}/support`} render={() => <div>support</div>}/>
+                <Route path={`${url}/block`} render={() => <div>block</div>}/>
+                <Route path={`${url}/block`} render={() => <div>block</div>}/>
+                <Route path={`${url}/authors`} render={() => <div>authors</div>}/>
             </Switch>
 
         </div>
