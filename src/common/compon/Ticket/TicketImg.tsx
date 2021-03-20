db9534df34a60a88777a48de10f8161cf0874089
sanya07 <sasha.kushnir.uz@gmail.com>
2021-03-20 22:38:39 +0200

    tickets

--- src/common/compon/Ticket/TicketImg.tsx
+++ src/common/compon/Ticket/TicketImg.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ticketimg from '../../images/TicketIcon.png'
 import s from './TicketImg.module.css'
+import { useDispatch } from "react-redux";
+import { message } from "antd";
+import { servicesActions } from "../../../redux/services/servicesActions";
+import { ProductCategoriesItem } from "../../../redux/newBanknote/newBanknoteReducer";
+import { ticketsActions } from "../../../redux/tickets/ticketsActions";
 
-export const TicketImg = () => {
+interface TicketImgProps {
+    ticketI: ProductCategoriesItem
+}
+export const TicketImg:React.FC<TicketImgProps> = ({ticketI}) => {
+    let [amount, setAmount]= useState(ticketI.amount?ticketI.amount:1)
+    const d = useDispatch()
+    const productI = {...ticketI}
+    const info = () => {
+        message.info('You have already selected this item',1);
+    };
+    const addTicket = () => {
+        if(!ticketI.showAmount) {
+            setAmount(amount as number + 1)
+            productI.amount = amount
+            d(ticketsActions.addTicketItem(ticketI,null))
+        } else {
+            info()
+        }
+    }
     return <div className={s.item}>
-        <img className={s.ticketimg} src={ticketimg} alt="Ticket img"/>
+        <img onClick={addTicket} className={s.ticketimg} src={ticketimg} alt="Ticket img"/>
     </div>
 }
--- src/components/CreateNewWrapper/CreateNewTickets/CreateNewTickets.tsx
+++ src/components/CreateNewWrapper/CreateNewTickets/CreateNewTickets.tsx
@@ -3,19 +3,18 @@ import { useSelector } from "react-redux";
 import { selectTickets } from "../../../selectors/selectCreateNew";
 import styles from './CreateNewTickets.module.css'
 import { TicketItemMap } from "./TicketItemMap";
-import { ProductCategoriesMyItem } from "../CreateNewMenus/MenuList/DishItem/ProductCategoriesMyItem";
 
 export const CreateNewTickets = () => {
 
     const tickets = useSelector(selectTickets).map((obj, index)=>
-        <ProductCategoriesMyItem key={index} keyVal={index}  showAmount={false} product_categoriesItem={obj}/>)
+        <TicketItemMap key={index}  showAmount={false} ticketItem={obj}/>)
 
     const selectedTickets = useSelector(selectTickets).map((obj, index)=>
-        <ProductCategoriesMyItem key={index} keyVal={index}  showAmount={true} product_categoriesItem={obj}/>)
+        <TicketItemMap key={index} showAmount={true} ticketItem={obj}/>)
 
     return <div className={styles.main}>
         <div className={styles.basket}>
-            {/*{selectedTickets}*/}
+            {selectedTickets}
         </div>
         <div className={styles.tickets}>
             {tickets}
