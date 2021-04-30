45f06380674519443826553897a43e26cf512c7f
Dima <dimhukys@gmail.com>
2021-04-30 23:26:19 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit f44d60a40c39196c7b5b6964532fe6366851d5a9.

--- src/components/CreateNewWrapper/CreateNewTickets/TicketItemMap.tsx
+++ src/components/CreateNewWrapper/CreateNewTickets/TicketItemMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import styles from './TicketItemMap.module.css'
 import { TicketImg } from "../../../common/compon/Ticket/TicketImg";
@@ -10,49 +10,9 @@ interface TicketItemProps {
     showAmount?: boolean
 }
 
-export const TicketItemMap: React.FC<TicketItemProps> = ({ticketItem, showAmount}) => {
-    const textInput = React.createRef<HTMLInputElement>()
-    const inputEl = useRef<HTMLInputElement>(null)
-
-    const d = useDispatch()
-    useEffect(() => {
-        if (ticketItem.showAmount) {
-            textInput.current?.focus()
-        }
-    }, [])
-    const deleteItem = () => {
-        d(ticketsActions.deleteFullTicketItem(ticketItem))
-    }
-    const changeCurT = (value: number) => {
-        d(ticketsActions.addTicketItem(ticketItem, value))
-    }
-    const [id] = useState(_uniqueId('prefix-'))
-
+export const TicketItemMap: React.FC<TicketItemProps> = ({ticketItem,showAmount}) => {
     return <div>
         <div className={styles.ticket}>
-            <div className={styles.img}>
-                <TicketImg ticketI={ticketItem}/>
-            </div>
-            <div className={styles.item}>
-                <div className={styles.title}>
-                    <div className={styles.name}>
-                        {ticketItem.name}
-                    </div>
-                    <div className={styles.price}>
-                        {ticketItem.price}$
-                    </div>
-                </div>
-                <button onClick={deleteItem} className={styles.btn}>Delete</button>
-                <div className={styles.input}>
-                    <label htmlFor={"def"} className={styles.text}>Amount</label>
-                    <NumericInput value={ticketItem.amount ? String(ticketItem.amount) : ""} onChange={changeCurT}/>
-                </div>
-                <div className={styles.desc}>
-                    {/*{ticketItem.description}*/}
-                </div>
-            </div>
-        </div>}
-        {!showAmount &&  <div className={styles.ticket}>
             <div className={styles.img}>
                 <TicketImg ticketI={ticketItem}/>
             </div>
